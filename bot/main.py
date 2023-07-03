2df15fb29672a78ca187cc83952ab3d7fd84a237
Ice Wolfy <icebluewolf05@gmail.com>
2023-07-02 20:33:46 -0500

    Get debug_guilds From Environment Variable

--- bot/main.py
+++ bot/main.py
@@ -5,10 +5,16 @@ from utils.bot import SurveyWolf
 COGS = ["utility", "survey"]
 
 intents = discord.Intents.default()
+
+try:
+    debug_guilds = [os.environ["debug_guilds"]]
+except KeyError:
+    debug_guilds = None
+
 bot = SurveyWolf(description="Make Surveys To Get Quick Opinions And Data",
                  intents=intents,
-                 debug_guilds=[678359965081141286])
-
+                 debug_guilds=debug_guilds
+                 )
 for cog in COGS:
     bot.load_extension(f"cogs.{cog}", store=False)
 
