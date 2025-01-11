d31d736adda80e7c91624e22610c6477076efd59
Ice Wolfy <icebluewolf05@gmail.com>
2025-01-11 12:29:18 -0500

    feat: Update Info Command Information

--- bot/cogs/utility.py
+++ bot/cogs/utility.py
@@ -16,32 +16,34 @@ class Utility(discord.Cog):
 
     @slash_command()
     async def info(self, ctx):
-        # Totally Not Made By ChatGPT
-        desc = (
-            "Survey Wolf is designed to make survey creation and hosting a breeze. With Survey Wolf, users can "
-            "easily create and customize surveys within Discord, allowing for easy access and engagement. The bot "
-            "is almost fully customizable, allowing users to tailor surveys to their specific needs and "
-            "preferences. Whether you're conducting market research, gathering feedback, or just wanting to get a "
-            "better understanding of your community's preferences, Survey Wolf makes it easy to get the insights "
-            "you need. With its user-friendly interface and customizable options, Survey Wolf is the perfect tool "
-            "for any Discord server looking to engage its members in meaningful conversations. "
-        )
+        desc = ("Survey Wolf is a highly customizable application to offer surveys directly on Discord.\n"
+                "- To create your first survey use the `/create` command.\n"
+                "- To send this survey use the `/send` command.\n"
+                "- To view the results use the `/results` command.\n"
+                "Survey Wolf aims to offer an easy to use interface to allow Discord server managers to collect "
+                "feedback about their members. If you have any specific requests please join the support server and "
+                "we will do our best to assist you.")
 
         e = await ef.general(
             title=self.bot.user.name
-            + (f"#{self.bot.user.discriminator}" if self.bot.user.discriminator != "0" else ""),
+                  + (f"#{self.bot.user.discriminator}" if self.bot.user.discriminator != "0" else ""),
             message=desc,
         )
         e.add_field(
             name="Support",
             value="If You Have An Issue With The Bot Please Join The Support Server [Here]("
-            'https://discord.gg/f39cJ9D/ "Survey Wolf Official Support Server")',
+                  'https://discord.gg/f39cJ9D/ "Survey Wolf Official Support Server")',
         )
         e.add_field(name="Credits", value="Main Bot Developer: icewolfy (451848182327148554)")
-        e.add_field(name="Technical", value="Uses Python V3 And Pycord V2. Database: PostgreSQL")
+        e.add_field(name="Technical", value="Uses Python V3 And [Pycord](https://pycord.dev/) V2. Database: PostgreSQL")
         e.add_field(
             name="Hosting",
-            value="Hosted On *To Be Determined*",
+            value="Hosted On Digital Ocean",
+        )
+        e.add_field(
+            name="Documents",
+            value="""[Privacy Policy](https://gist.github.com/Icebluewolf/90335bbc4d82d435d437b5da98f71df6)
+            [Terms Of Service](https://gist.github.com/Icebluewolf/7e73be418408ac48a35deb8045ae2a29)"""
         )
         await ctx.respond(embed=e, ephemeral=True)
 
