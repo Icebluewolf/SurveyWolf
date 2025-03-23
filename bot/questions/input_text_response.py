5565921d8972716d273d118f5022ac0979d82058
Ice Wolfy <icebluewolf05@gmail.com>
2025-03-23 16:36:52 -0400

    feat: Ability To Have Errors And Retry InputText Responses

--- bot/questions/input_text_response.py
+++ bot/questions/input_text_response.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from typing import Self
 
 import discord.ui
+from discord import Interaction
+from utils import embed_factory as ef
 
 from questions.survey_question import SurveyQuestion
 
@@ -33,6 +35,18 @@ class InputTextResponse(SurveyQuestion, ABC):
         return modal.interaction
 
 
+class RetryButton(discord.ui.Button):
+    interaction: discord.Interaction
+
+    def __init__(self, retry: list[InputTextResponse]):
+        super().__init__(label="Click To Fix The Errors")
+        self.retry = retry
+
+    async def callback(self, interaction: Interaction):
+        self.interaction = await self.retry[0].send_question(interaction, self.retry)
+        self.view.stop()
+
+
 class GetResponse(discord.ui.Modal):
     def __init__(self, questions: list[InputTextResponse]):
         super().__init__(title="Type Your Answer Below")
@@ -43,10 +57,19 @@ class GetResponse(discord.ui.Modal):
 
     async def callback(self, interaction: discord.Interaction):
         self.interaction = interaction
-        errors = []
+        errors: list[str] = []
+        retry: list[InputTextResponse] = []
         for n, question in enumerate(self.questions):
             e = await question.handle_input_text_response(self.children[n].value)
             if e is not None:
+                retry.append(question)
                 errors.append(e)
+        if retry:
+            b = RetryButton(retry)
+            v = discord.ui.View(b)
+            e = await ef.input_error("Some Questions Had Invalid Inputs", errors)
+            await interaction.response.send_message(embed=e, view=v, ephemeral=True)
+            await v.wait()
+            self.interaction = b.interaction
 
         self.stop()
